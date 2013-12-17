5ef96649225b04943723bc13a3e6a3f3aa479562
Marcin Glombiowski <s.holmes120@wp.pl>
2013-12-17 13:15:38 +0100

    mutacja jest, koncepcja zaakceptowana przez Wierzcha

--- src/pl/ug/edu/evo/grid/Centroid.java
+++ src/pl/ug/edu/evo/grid/Centroid.java
@@ -113,19 +113,56 @@ public class Centroid extends Point implements GeneticCentroid {
   }
 
 
+private List<Double> calculateBreedCoords(Point far) {
+	List<Double> coords = new ArrayList<Double>();
+	Random r = new Random(); 
+	
+	for(int i=0; i<this.getCoordinateList().size(); i++) {
+
+		coords.add(r.nextGaussian() * ((getPointsInCluster().size()-1) * (this.getCoordinateList().get(i)) + far.getCoordinateList().get(i))/getPointsInCluster().size());
+
+	}
+	
+	
+	return coords;
+}
 
 @Override
 public Centroid mutate() {
-	List<Double> coords = new ArrayList<Double>();
+	
+	// Tworzy nowe punkty centroidu, przesuwa o iloczyn random.gaussa razy wa¿ony sredni dystans pomiêdzy centroidem a najdalszym punktem
+	
+	
+	//tl;dr 
+	// Kalkuluje nowy punkt pomiêdzy centoridem a najdalszym punktem i przemnza jego coordy przez random
+	
+	//Point nearestPoint = null;
+	Point furthestPoint = null;
+	//double minVal = Double.NaN;
+	double maxVal = Double.NaN;
+	double dist = Double.NaN;
 
-	for(Double d : this.getCoordinateList()) {
-		double rand = (Math.random()*4)-2;
-		coords.add(d*rand);/////////////////////////////////////////////////////
+	for(Point d : pointsInCluster) {
+		dist = Point.distance(d, this);
+
+		if(Double.isNaN(maxVal)) {
+			//minVal = dist;
+			maxVal = dist;
+			//nearestPoint = d;
+			furthestPoint = d;
+		//} else if(minVal>dist) {
+		//	nearestPoint = d;
+		//	minVal = dist;
+		} else if(maxVal<dist) {
+			furthestPoint = d;
+			maxVal = dist;
+		}
 		
 	}
 		
 
-	Centroid breed = new Centroid(coords);
+	Centroid breed = new Centroid(calculateBreedCoords(furthestPoint));
 	return breed;
 }
+
 }
